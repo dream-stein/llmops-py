005a0efd7e02632ac0d88a9a7c8f4bcd2309e9cb
emcikem <2287697454@qq.com>
2025-06-09 00:39:51 +0800

    feat(.service): 通用异常

--- internal/exception/__init__.py
+++ internal/exception/__init__.py
@@ -5,3 +5,20 @@
 #Author  :Emcikem
 @File    :__init__.py.py
 """
+from .exception import (
+    CustomException,
+    FailException,
+    NotFoundException,
+    UnauthorizedException,
+    ForbiddenException,
+    ValidateErrorException,
+)
+
+__all__ = [
+    "CustomException",
+    "FailException",
+    "NotFoundException",
+    "UnauthorizedException",
+    "ForbiddenException",
+    "ValidateErrorException",
+]
