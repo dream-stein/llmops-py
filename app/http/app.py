810bec1dcba42f06947ac00d8e94e5ad20ea1267
emcikem <2287697454@qq.com>
2025-06-10 00:59:15 +0800

    feat(.service): mysql连接

--- app/http/app.py
+++ app/http/app.py
@@ -6,20 +6,23 @@
 @File    :app.py
 """
 import dotenv
-from injector import Injector
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 from internal.router import Router
 from internal.server import Http
+from .module import injector
 
-# 将env加载到环境变量中
+# 1.将env加载到环境变量中
 dotenv.load_dotenv()
+
+# 2.构建LLMOps项目配置
 conf = Config()
-injector = Injector()
 
 app = Http(
     __name__,
     conf=conf,
+    db=injector.get(SQLAlchemy),
     router=injector.get(Router),
 )
 if __name__ == "__main__":
--- internal/server/http.py
+++ internal/server/http.py
@@ -13,12 +13,17 @@ from config import Config
 from internal.exception import CustomException
 from internal.router import Router
 from pkg.response import Response, json, HttpCode
-
+from flask_sqlalchemy import SQLAlchemy
 
 class Http(Flask):
     """Http服务引擎"""
 
-    def __init__(self, *args, conf: Config, router: Router, **kwargs):
+    def __init__(self,
+                 *args,
+                 conf: Config,
+                 db: SQLAlchemy,
+                 router: Router,
+                 **kwargs):
         # 1.调用父类构造函数初始化
         super().__init__(*args, **kwargs)
 
@@ -28,7 +33,10 @@ class Http(Flask):
         # 3.注册绑定异常错误处理
         self.register_error_handler(Exception, self._register_error_handler)
 
-        # 4.注册应用路由
+        # 4. 初始化flask扩展
+        db.init_app(self)
+
+        # 5.注册应用路由
         router.register_router(self)
 
     def _register_error_handler(self, error: Exception):
