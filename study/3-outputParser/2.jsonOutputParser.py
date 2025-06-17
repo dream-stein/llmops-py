dcca729e539d44d27c9dae21d037a583fcee2a8c
emcikem <2287697454@qq.com>
2025-06-18 00:25:04 +0800

    feat(.service): 练习

---
+++ study/3-outputParser/2.jsonOutputParser.py
@@ -0,0 +1,28 @@
+#!/usr/bin/eny python
+# -*- coding: utf-8 -*-
+"""
+@Time    :2025/6/18 00:04
+#Author  :Emcikem
+@File    :1.StrOutputParser.py
+"""
+import dotenv
+from click import prompt
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.output_parsers import JsonOutputParser
+
+dotenv.load_dotenv()
+
+class Joke(BaseModel):
+    joke: str = Field(description="回答用户的冷笑话")
+    punchline: str = Field(description="这个冷笑话的笑点")
+
+parser = JsonOutputParser(pydantic_object=Joke)
+prompt = ChatPromptTemplate.from_template("请根据用户提问回答。\n{format_instructions}\n {query}").partial(format_instructions=parser.get_format_instructions())
+
+llm = ChatOpenAI(model="deepseek-chat")
+
+joke = parser.invoke(llm.invoke(prompt.invoke({"query": "请讲一个关于程序员的冷笑话"})))
+print(joke)
