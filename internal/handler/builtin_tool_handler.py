f1a3c775517f7e7221364417e91685b6a5369464
emcikem <2287697454@qq.com>
2025-07-05 20:20:57 +0800

    feat(.service): 内置插件路由注册

---
+++ internal/handler/builtin_tool_handler.py
@@ -0,0 +1,22 @@
+#!/usr/bin/eny python
+# -*- coding: utf-8 -*-
+"""
+@Time    :2025/7/5 20:10
+#Author  :Emcikem
+@File    :builtin_tool_handler.py
+"""
+from injector import inject
+from dataclasses import dataclass
+
+@inject
+@dataclass
+class BuiltinToolHandler:
+    """内置工具处理器"""
+
+    def get_builtin_tools(self):
+        """获取LLMOps所有内置工具信息+提供商信息"""
+        pass
+
+    def get_provider_tool(self, provider: str, tool: str):
+        """根据传递的提供商名字+工具名字获取指定工具的信息"""
+        pass
--- internal/router/router.py
+++ internal/router/router.py
@@ -9,13 +9,14 @@ from dataclasses import dataclass
 from injector import inject
 from flask import Flask, Blueprint
 
-from internal.handler import AppHandler
+from internal.handler import AppHandler, BuiltinToolHandler
 
 @inject
 @dataclass()
 class Router:
     """路由"""
     app_handler: AppHandler
+    builtin_tool_handler: BuiltinToolHandler
 
     def register_router(self, app: Flask):
         """注册路由"""
@@ -24,13 +25,15 @@ class Router:
 
         # 2. 将url与对应控制器方法做绑定
         bp.add_url_rule("/ping", view_func=self.app_handler.ping)
-        bp.add_url_rule("/apps/<uuid:app_id>/debug", methods=["POST", "OPTIONS"], view_func=self.app_handler.debug)
-        # bp.add_url_rule("/app", methods=["POST"], view_func=self.app_handler.create_app)
-        # bp.add_url_rule("/app/<uuid:id>", view_func=self.app_handler.get_app)
-        # bp.add_url_rule("/app/<uuid:id>", methods=["POST"], view_func=self.app_handler.update_app)
-        # bp.add_url_rule("/app/<uuid:id>", methods=["DELETE"], view_func=self.app_handler.delete_app)
+        bp.add_url_rule("/apps/<uuid:app_id>/debug", methods=["POST"], view_func=self.app_handler.debug)
+        bp.add_url_rule("/app", methods=["POST"], view_func=self.app_handler.create_app)
+        bp.add_url_rule("/app/<uuid:id>", view_func=self.app_handler.get_app)
+        bp.add_url_rule("/app/<uuid:id>", methods=["POST"], view_func=self.app_handler.update_app)
+        bp.add_url_rule("/app/<uuid:id>", methods=["DELETE"], view_func=self.app_handler.delete_app)
 
+        # 3.内置插件广场模块
+        bp.add_url_rule("/builtin_tools", view_func=self.builtin_tool_handler.get_builtin_tools)
+        bp.add_url_rule("/")
 
-
-        # 3. 在应用上去注册蓝图
+        # 4. 在应用上去注册蓝图
         app.register_blueprint(bp)
