8db2abc11314133dfd6cf718ea9ae4373fd28825
emcikem <2287697454@qq.com>
2025-08-23 18:19:36 +0800

    feat(.service): 简化代码

--- internal/lib/helper.py
+++ internal/lib/helper.py
@@ -6,11 +6,14 @@
 @File    :helper.py
 """
 from datetime import datetime
+from enum import Enum
 from hashlib import sha3_256
 import importlib
 from typing import Any
+from uuid import UUID
 
 from langchain_core.documents import Document
+from pydantic import BaseModel
 
 
 def dynamic_import(module_name: str, symbol_name: str) -> Any:
@@ -42,10 +45,52 @@ def datetime_to_timestamp(dt: datetime) -> int:
         return 0
     return int(dt.timestamp())
 
+def combine_documents(documents: list[Document]) -> str:
+    return "\n\n".join([document.page_content for document in documents])
+
 def remove_fields(data_dict: dict, fields: list[str]) -> None:
     """根据传递的字段名移除字段中指定的字段"""
     for field in fields:
         data_dict.pop(field, None)
 
-def combine_documents(documents: list[Document]) -> str:
-    return "\n\n".join([document.page_content for document in documents])
+def convert_model_to_dict(obj: Any, *args, **kwargs):
+    """辅助函数，将pydantic V1版本中的UUID/Enum等数据转换成克序列化存储的数据"""
+    # 1.让给是Pydantic的BaseModel类型，递归处理其字段
+    if isinstance(obj, BaseModel):
+        obj_dict = obj.model_dump(*args, **kwargs)
+        # 2.递归处理嵌套字段
+        for key, value in obj_dict.items():
+            obj_dict[key] = convert_model_to_dict(value, *args, **kwargs)
+        return obj_dict
+
+    # 3.如果是 UUID 类型，转化为字符串
+    elif isinstance(obj, UUID):
+        return str(obj)
+
+    # 4.如果是 Enum 类型，转换为其值
+    elif isinstance(obj, Enum):
+        return obj.value
+
+    # 5.如果是列表类型，递归处理列表中的每个元素
+    elif isinstance(obj, list):
+        return [convert_model_to_dict(value, *args, **kwargs) for value in obj]
+
+    # 6.如果是字典类型，递归处理字典中的每个字段
+    elif isinstance(obj, dict):
+        return {convert_model_to_dict(value, *args, **kwargs) for key, value in obj.items()}
+
+    # 7.对其他类型的字段，保持原样
+    return obj
+
+def get_value_type(value: Any) -> Any:
+    """根据传递的值获取变量的类型，并将str和bool转换成string和boolean"""
+    # 1.计算变量的类型并转换成字符串
+    value_type = type(value).__name__
+
+    # 2.判断是否为str或者bool
+    if value_type == "str":
+        return "string"
+    elif value_type == "bool":
+        return "boolean"
+
+    return value_type
--- internal/service/langguage_model_service.py
+++ internal/service/langguage_model_service.py
@@ -15,6 +15,7 @@ from dataclasses import dataclass
 
 from internal.core.language_model import LanguageModelManager
 from internal.exception import NotFoundException
+from internal.lib.helper import convert_model_to_dict
 from internal.service import BaseService
 from pkg.sqlalchemy import SQLAlchemy
 
@@ -47,27 +48,7 @@ class LanguageModelService(BaseService):
                 "description": provider_entity.description,
                 "background": provider_entity.background,
                 "support_model_types": provider_entity.supported_model_types,
-                "models": [{
-                    "model": model_entity.model_name,
-                    "label": model_entity.label,
-                    "model_type": model_entity.model_type,
-                    "context_window": model_entity.context_window,
-                    "max_output_tokens": model_entity.max_output_tokens,
-                    "features": model_entity.features,
-                    "attributes": model_entity.parameters,
-                    "parameters": [{
-                        "name": parameter.name,
-                        "label": parameter.label,
-                        "type": parameter.type.value,
-                        "help": parameter.help,
-                        "required": parameter.required,
-                        "default": parameter.default,
-                        "min": parameter.min,
-                        "max": parameter.max,
-                        "precision": parameter.precision,
-                        "options": [{"label": option.label, "value": option.value} for option in parameter.options],
-                    } for parameter in model_entity.parameters]
-                } for model_entity in model_entities],
+                "models": convert_model_to_dict(model_entities),
             }
             language_models.append(language_model)
 
@@ -86,26 +67,7 @@ class LanguageModelService(BaseService):
             raise NotFoundException("该模型不存在")
 
         # 3.构建数据并响应
-        language_model = {
-            "model": model_entity.model_name,
-            "label": model_entity.label,
-            "model_type": model_entity.model_type,
-            "context_window": model_entity.context_window,
-            "max_output_tokens": model_entity.max_output_tokens,
-            "features": model_entity.features,
-            "attributes": model_entity.parameters,
-            "metadata": model_entity.metadata,
-            "parameters": [{
-                "name": parameter.name,
-                "label": parameter.label,
-                "type": parameter.type.value,
-                "help": parameter.help,
-                "required": parameter.required,
-                "options": [{"label": option.label, "value": option.value} for option in parameter.options],
-            } for parameter in model_entity.parameters],
-        }
-
-        return language_model
+        return convert_model_to_dict(model_entity)
 
     def get_language_model_icon(self, provider_name: str) -> tuple[bytes, str]:
         """根据传递的提供者名字获取提供商对应的图标信息"""
