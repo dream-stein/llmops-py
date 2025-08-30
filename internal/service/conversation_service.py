acda802a70f45f56c6ceb0b80a47da4f7adecf0c
emcikem <2287697454@qq.com>
2025-08-30 21:40:46 +0800

    feat(.service): 数据分析

--- internal/service/conversation_service.py
+++ internal/service/conversation_service.py
@@ -192,8 +192,19 @@ class ConversationService(BaseService):
                     # 8.更新消息信息
                     self.update(
                         message,
+                        # 消息相关字段
                         message=agent_thought.message,
+                        message_token_count=agent_thought.message_token_count,
+                        message_unit_price=agent_thought.message_unit_price,
+                        message_price_unit=agent_thought.message_price_unit,
+                        # 答案相关字段
                         answer=agent_thought.answer,
+                        answer_token_count=agent_thought.answer_token_count,
+                        answer_unit_price=agent_thought.answer_unit_price,
+                        answer_price_unit=agent_thought.answer_price_unit,
+                        # Agent推理统计相关
+                        total_token_count=agent_thought.total_token_count,
+                        total_price=agent_thought.total_price,
                         latency=latency,
                     )
 
