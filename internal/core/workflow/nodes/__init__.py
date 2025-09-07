658555d6272908365c6d7bbf4bb39fe7829f4161
emcikem <2287697454@qq.com>
2025-09-07 13:45:05 +0800

    feat(.service): 工作流模块校验

--- internal/core/workflow/nodes/__init__.py
+++ internal/core/workflow/nodes/__init__.py
@@ -6,21 +6,31 @@
 @File    :__init__.py.py
 """
 from .base_node import BaseNode
+from .code.code_entity import CodeNodeData
+from .dataset_retrival.dataset_retrieval_entity import DatasetRetrievalNodeData
+from .end.end_entity import EndNodeData
+from .http_request.http_request_entity import HttpRequestNodeData
+from .http_request.http_request_node import HttpRequestNode
+from .llm.llm_entity import LLMNodeData
+from .start.start_entity import StartNodeData
 from .start.start_node import StartNode
 from .end.end_node import EndNode
 from .llm.llm_node import LLMNode
+from .template_transform.template_transform_entity import TemplateTransformNodeData
 from .template_transform.template_transform_node import TemplateTransformNode
 from .dataset_retrival.dataset_retrieval_node import DatasetRetrievalNode
 from .code.code_node import CodeNode
+from .tool.tool_entity import ToolNodeData
 from .tool.tool_node import ToolNode
 
 __all__ = [
     "BaseNode",
-    "StartNode",
-    "EndNode",
-    "LLMNode",
-    "TemplateTransformNode",
-    "DatasetRetrievalNode",
-    "CodeNode",
-    "ToolNode",
+    "StartNode", "StartNodeData",
+    "LLMNode", "LLMNodeData",
+    "TemplateTransformNode", "TemplateTransformNodeData",
+    "DatasetRetrievalNode", "DatasetRetrievalNodeData",
+    "CodeNode", "CodeNodeData",
+    "ToolNode", "ToolNodeData",
+    "HttpRequestNode", "HttpRequestNodeData",
+    "EndNode", "EndNodeData",
 ]
