edfcd2ba9ecb278b301e520dd792b40ab4c454d4
emcikem <2287697454@qq.com>
2025-09-16 00:01:09 +0800

    fix(.service): 工具调用修改

--- internal/core/tools/builtin_tools/providers/time/current_time.py
+++ internal/core/tools/builtin_tools/providers/time/current_time.py
@@ -9,20 +9,25 @@ from datetime import datetime
 from typing import Any, Type
 
 from langchain_core.tools import BaseTool
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+from internal.lib.helper import add_attribute
+
+
+class CurrentArgsSchema(BaseModel):
+    pass
 
 class CurrentTimeTool(BaseTool):
     """一个用于获取当前时间的工具"""
     name: str = "current_time"
     description: str = "一个用于获取当前时间的工具"
-    args_schema: Type[BaseModel] = BaseModel
+    args_schema: Type[BaseModel] = CurrentArgsSchema
 
     def _run(self, *args: Any, **kwargs: Any) -> Any:
         """获取当前系统的时间并进行格式化后返回"""
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z")
 
-
+@add_attribute("args_schema", CurrentArgsSchema)
 def current_time(**kwargs) -> BaseTool:
     """返回获取当前时间的LangChain工具"""
     return CurrentTimeTool()
