272606f8b0e2f1c4eac9dfb4d342c6db85974ff8
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-09-30 10:30:30 +0800

    Update celery_extension.py

--- internal/extension/celery_extension.py
+++ internal/extension/celery_extension.py
@@ -11,7 +11,7 @@ from flask import Flask
 def init_app(app: Flask):
     """Celery配置服务初始化"""
 
-    class FakeTask(Task):
+    class FlaskTask(Task):
         """定义FlaskTask，确保Celery在Flask应用的上下文中运行，这样可以访问flask配置、数据库等内容"""
 
         def __call__(self, *args, **kwargs):
@@ -19,8 +19,9 @@ def init_app(app: Flask):
                 return self.run(*args, **kwargs)
 
     # 1.创建Celery应用并配置
-    celery_app = Celery(app.name, task_cls=FakeTask)
+    celery_app = Celery(app.name, task_cls=FlaskTask)
     celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
 
     # 将celery挂在到app的扩展中
     app.extensions["celery"] = celery_app
