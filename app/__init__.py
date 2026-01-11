06a8bb71aa14bbaf493e588b04fde3b182874a43
wx40217 <yps302@163.com>
2026-01-11 16:46:11 +0800

    init

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2026/1/11 16:39
+@Author  : yps302@163.com
+@File    : __init__.py.py
+"""
---
+++ internal/task/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2026/1/11 16:36
+@Author  : yps302@163.com
+@File    : __init__.py.py
+"""
