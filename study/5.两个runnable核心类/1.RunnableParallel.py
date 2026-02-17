34ddbf4511d6ad517445ed6304c752213553b2e9
wx40217 <yps302@163.com>
2026-02-17 21:56:48 +0800

    RunnableParallel

---
+++ study/5.两个runnable核心类/1.RunnableParallel.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2026/2/17 21:29
+@Author  : yps302@163.com
+@File    : 1.RunnableParallel.py
+"""
+import dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel
+from langchain_openai import ChatOpenAI
+
+dotenv.load_dotenv()
+# 1.编排prompt
+joke_prompt = ChatPromptTemplate.from_template("请讲一个关于{subject}的冷笑话")
+poem_prompt = ChatPromptTemplate.from_template("请写一篇关于{subject}的诗")
+
+# 2.创建llm
+llm = ChatOpenAI(model="LongCat-Flash-Lite", )
+
+# 3.创建输出解析器
+parser = StrOutputParser()
+
+# 4.编排链
+joke_chain = joke_prompt | llm | parser
+poem_chain = poem_prompt | llm | parser
+
+# 5.并行链
+map_chain = RunnableParallel(joke=joke_chain, poem=poem_chain)
+# map_chain = RunnableParallel({
+#     "joke": joke_chain,
+#     "poem": poem_chain,
+# })
+
+res = map_chain.invoke({"subject": "程序员"})
+print(res)
---
+++ study/5.两个runnable核心类/2.RunnableParallel模拟检索.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2026/2/17 21:39
+@Author  : yps302@163.com
+@File    : 2.RunnableParallel模拟检索.py
+"""
+from operator import itemgetter
+
+import dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+dotenv.load_dotenv()
+
+
+def retrieval(query: str) -> str:
+    """一个模拟的检索器函数"""
+    print(query, "正在检索")
+    return "我是野兽先辈"
+
+
+# 1.编排prompt
+prompt = ChatPromptTemplate.from_template("""请根据用户的问题回答，可以参考对应的上下文进行生成
+
+<context>
+{context}
+</context>
+
+用户的提问是：{query}""")
+
+# 2.创建llm
+llm = ChatOpenAI(model="LongCat-Flash-Lite", )
+
+# 3.创建输出解析器
+parser = StrOutputParser()
+
+# 4.编排链
+chain = {
+            # "context": lambda x: retrieval(x["query"]),
+            # "query": lambda x: x["query"],
+            "context": lambda x: retrieval(x["query"]),
+            "query": itemgetter("query"),
+        } | prompt | llm | parser  # 在链中使用不需要声明RunnableParallel 管道运算符会进行转换
+
+# 5.调用链
+content = chain.invoke({"query": "你好，我是谁"})
+print(content)
